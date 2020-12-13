dc8f13cb821325ea4d9502a1489ce6b6f0a2d4af
folly77folly <folly77folly@gmail.com>
2020-12-13 11:17:58 +0100

    added-reponsivness

--- app/Traits/AccessTrait.php
+++ app/Traits/AccessTrait.php
@@ -22,6 +22,19 @@ trait AccessTrait
             'pin' => $validatedData['pin']
         ];
 
+        $message = "A new user is added";
+        $email= $data['email'];
+        $details = [
+            'title' => 'Thanks For Contacting Me ',
+            'body' => 'I Appreciate your Message and will reply Shortly'
+        ];
+    
+        \Mail::to($email)->send(new \App\Mail\MessageMail($details));
+        $details = [
+            'title' => 'A New Contact ',
+            'body' => $message
+        ];
+
         Access::create($data);
         Log::info('Email Sent');
     }
